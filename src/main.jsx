8b1eeb848b18b469f2fcf4cfb761fa967dff0734
Robert Bailey <r.bail@hotmail.com>
2024-02-03 18:56:30 +1030

    some of app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import 'animate.css/animate.min.css';
 import App from './App';
 //or import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+//another version (below)?
+//ReactDOM.render(
+    //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  //document.getElementById('root')
+//);
