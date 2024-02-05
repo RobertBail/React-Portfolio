4390c331983c08ac97b58d7fca0910bf39c7c020
Robert Bailey <r.bail@hotmail.com>
2024-02-06 03:16:13 +1030

    navbar working better

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,11 +1,11 @@
-export default function NavTabs(currentPage, handlePageChange) {
+export default function NavTabs({currentPage, handlePageChange}) {
     // The Navbar UI component will render each of the Link elements in the links prop
     return (
       <ul className="nav nav-tabs">
        
         <li className="nav-item">
           <a
-            href="https://rb-react-portfolio.netlify.app/#about"
+            href="#about"
             onClick={() => handlePageChange('About')}
             // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
@@ -15,7 +15,7 @@ export default function NavTabs(currentPage, handlePageChange) {
         </li>
         <li className="nav-item">
           <a
-            href="https://rb-react-portfolio.netlify.app/#portfolio"
+            href="#portfolio"
             onClick={() => handlePageChange('Portfolio')}
             className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
           >
@@ -24,7 +24,7 @@ export default function NavTabs(currentPage, handlePageChange) {
         </li>
         <li className="nav-item">
           <a
-            href="https://rb-react-portfolio.netlify.app/#resume"
+            href="#resume"
             onClick={() => handlePageChange('Resume')}
             // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
@@ -34,7 +34,7 @@ export default function NavTabs(currentPage, handlePageChange) {
         </li>
         <li className="nav-item">
           <a
-            href="https://rb-react-portfolio.netlify.app/#contact"
+            href="#contact"
             onClick={() => handlePageChange('Contact')}
             // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
